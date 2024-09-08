4736ef20b6817975543a6f34aa4aa52a6459d147
Lassi Koivisto <lassi.koivisto7@gmail.com>
2024-09-08 12:10:39 +0300

    Initial commit

---
+++ test/helloer.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Helloer from '../lib/helloer-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/helloer-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Helloer.HelloerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
